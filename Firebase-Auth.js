ca22c117d9d9a1c60d5c833318897f4a19a3f994
tubalrr <tubalrr@gmail.com>
2026-09-08 22:35:13 +0800

    Update Firebase-Auth.js

--- Firebase-Auth.js
+++ Firebase-Auth.js
@@ -1,17 +1,4 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyCjYzrOvJuV1UN67aljAHQEK5LWCmtvMPw",
-  authDomain: "payapang-isip.firebaseapp.com",
-  projectId: "payapang-isip",
-  storageBucket: "payapang-isip.firebasestorage.app",
-  messagingSenderId: "901078398408",
-  appId: "1:901078398408:web:1f4c5e5794f96801f57cfd"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
+import { getFirestore, collection, query, orderBy, onSnapshot } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-firestore.js";
+const app = initializeApp({apiKey:"AIzaSyCjYzrOvJuV1UN67aljAHQEK5LWCmtvMPw",authDomain:"payapang-isip.firebaseapp.com",projectId:"payapang-isip",storageBucket:"payapang-isip.firebasestorage.app",messagingSenderId:"901078398408",appId:"1:901078398408:web:1f4c5e5794f96801f57cfd"});
+const db = getFirestore(app);
