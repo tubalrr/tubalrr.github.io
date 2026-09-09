ee61c7659a8973763721db9cd47009e9072c5636
tubalrr <tubalrr@gmail.com>
2026-09-09 22:26:31 +0800

    Update Firebase-Auth.js

--- Firebase-Auth.js
+++ Firebase-Auth.js
@@ -5,7 +5,7 @@ import { getAuth, GoogleAuthProvider, signInWithPopup, signOut, onAuthStateChang
 import { getFirestore, doc, setDoc, serverTimestamp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-firestore.js";
 
 const firebaseConfig = {
- apiKey:"AIzaSyAZBTseLADkT-SMNMrRl0XgLkMe7LvvYG8",
+ apiKey:"AIzaSyCxiOo0BzOUgRcZZpUKVF3uu2ktjyjg8OM",
  authDomain:"payapang-isip.firebaseapp.com",
  projectId:"payapang-isip",
  storageBucket:"payapang-isip.firebasestorage.app",
