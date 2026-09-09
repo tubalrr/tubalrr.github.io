78c2a8cedbf454644c2830486a7268782fbcd4a8
tubalrr <tubalrr@gmail.com>
2026-09-09 22:57:39 +0800

    Add files via upload

---
+++ Firebase-Config-Example.js
@@ -0,0 +1,10 @@
+// Copy this to firebase-config.js and fill your real key
+// firebase-config.js is in .gitignore - never commit it
+export const firebaseConfig = {
+  apiKey: "YOUR_API_KEY_HERE",
+  authDomain: "payapang-isip.firebaseapp.com",
+  projectId: "payapang-isip",
+  storageBucket: "payapang-isip.firebasestorage.app",
+  messagingSenderId: "901078398408",
+  appId: "1:901078398408:web:1f4c5e5794f96801f57cfd"
+};
